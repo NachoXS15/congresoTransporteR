edb8792f174624eec0ada561bf4c43151d00ddd0
StJemmy <87050563+StJemmy@users.noreply.github.com>
2024-08-13 21:36:07 -0300

    actualización minima para nacho, creando nueva branch

--- src/components/ui/Patreon.tsx
+++ src/components/ui/Patreon.tsx
@@ -1,19 +1,27 @@
-import { PatreonProp } from "../../config/types"
-export default function Patreon({nombre, img, alt, link}: PatreonProp) {
-    return (
-        <div className="">
-            <div className="aspect-square w-24 md:w-32 bg-white dark:bg-gray-400 rounded-full group grid place-content-center relative overflow-hidden">
-                <div className="absolute w-full h-full z-0 group-hover:opacity-15 grid place-content-center px-2">
-                    {
-                        img ?
-                            <img src={'../../src/assets/img/sponsors/' + img} alt={alt} className="drop-shadow-md"/>
-                            :
-                            <img src="assets/img/300_300.png" alt='no hay imagen' />
-                    }
-                </div>
-                <a href={link} className="text-transparent transition-all translate-y-5 group-hover:translate-y-0 group-hover:text-gray-950">{nombre}</a>
-            </div>
-
+import { PatreonProp } from "../../config/types";
+export default function Patreon({ nombre, img, alt, link }: PatreonProp) {
+  return (
+    <div className="">
+      <div className="w-24 h-32 md:w-32 rounded-full group grid place-content-center relative overflow-hidden">
+        <div className="absolute w-full h-full z-0 group-hover:opacity-10 grid place-content-center px-2 bg-slate-500">
+          {img ? (
+            <img
+              src={"../../src/assets/img/sponsors/" + img}
+              alt={alt}
+              //   className="drop-shadow-[0_2px_1px_rgba(255,255,255,0.5)]"
+            />
+          ) : (
+            <img src="assets/img/300_300.png" alt="no hay imagen" />
+          )}
         </div>
-    )
+        <a
+          href={link}
+          target="_blank"
+          className="text-transparent transition-all translate-y-5 group-hover:translate-y-0 group-hover:text-gray-100 "
+        >
+          {nombre}
+        </a>
+      </div>
+    </div>
+  );
 }
--- src/config/data.ts
+++ src/config/data.ts
@@ -1,99 +1,98 @@
 interface members {
-    id: number;
-    nombre: string;
-    rol?: string;
-    extra?: string;
-    img?: string;
-  }
-  interface sponsor {
-    id: number;
-    nombre: string;
-    url: string;
-    img?: string;
-  }
-  
-  interface item {
-    id: number;
-    info: string;
-    hora?: string;
-    tag?: string;
-  }
-  export const cronograma: item[] = [
-    {
-      id: 1,
-      info: "Acreditación",
-      hora: "09:00 am",
-    },
-    {
-      id: 2,
-      info: "Acreditación",
-      hora: "09:30 am",
-    },
-    {
-      id: 3,
-      info: "Acreditación",
-      hora: "09:00 am",
-      tag: "exposición",
-    },
-  ];
-  
-  export const directivos: members[] = [
-    {
-      id: 1,
-      nombre: "Marcelo Ferraris",
-      rol: "Presidente",
-      extra: "Provincia de Salta",
-    },
-    {
-      id: 2,
-      nombre: "Alcira Brizuela",
-      rol: "Vicepresidente",
-      extra: "Provincia de La Rioja",
-    },
-    {
-      id: 3,
-      nombre: "Juan Ignacio Ciancaglini",
-      rol: "Secretario",
-      extra: "Provincia de Rio Negro",
-    },
-  ];
-  
-  export const sponsors: sponsor[] = [
-    {
-      id: 1,
-      nombre: "Mercedes-Benz",
-      url: "https://www.mercedes-benz.com.ar/",
-      img: "mercedes_logos.png",
-    },
-    {
-      id: 2,
-      nombre: "Bizland",
-      url: "https://www.bizland.tech/",
-      img: "bizland.png",
-    },
-    {
-      id: 3,
-      nombre: "Smod",
-      url: "https://bicicletas.smod.io/",
-      img: "smod.png",
-    },
-    {
-      id: 4,
-      nombre: "Marcopolo",
-      url: "https://www.marcopolo.com.br/es",
-      img: "marcopolo-logo.png",
-    },
-    {
-      id: 5,
-      nombre: "Red Bus",
-      url: "/",
-      img: "LogoRedBus.png",
-    },
-    {
-      id: 6,
-      nombre: "Buswagen",
-      url: "https://buswagen.com.ar/",
-      img: "buswagen_s_a_cover.jpg",
-    },
-  ];
-  
+  id: number;
+  nombre: string;
+  rol?: string;
+  extra?: string;
+  img?: string;
+}
+interface sponsor {
+  id: number;
+  nombre: string;
+  url: string;
+  img?: string;
+}
+
+interface item {
+  id: number;
+  info: string;
+  hora?: string;
+  tag?: string;
+}
+export const cronograma: item[] = [
+  {
+    id: 1,
+    info: "Acreditación",
+    hora: "09:00 am",
+  },
+  {
+    id: 2,
+    info: "Acreditación",
+    hora: "09:30 am",
+  },
+  {
+    id: 3,
+    info: "Acreditación",
+    hora: "09:00 am",
+    tag: "exposición",
+  },
+];
+
+export const directivos: members[] = [
+  {
+    id: 1,
+    nombre: "Marcelo Ferraris",
+    rol: "Presidente",
+    extra: "Provincia de Salta",
+  },
+  {
+    id: 2,
+    nombre: "Alcira Brizuela",
+    rol: "Vicepresidente",
+    extra: "Provincia de La Rioja",
+  },
+  {
+    id: 3,
+    nombre: "Juan Ignacio Ciancaglini",
+    rol: "Secretario",
+    extra: "Provincia de Rio Negro",
+  },
+];
+
+export const sponsors: sponsor[] = [
+  {
+    id: 1,
+    nombre: "Mercedes-Benz",
+    url: "https://www.mercedes-benz.com.ar/",
+    img: "mercedes_logos.png",
+  },
+  {
+    id: 2,
+    nombre: "Bizland",
+    url: "https://www.bizland.tech/",
+    img: "bizland.png",
+  },
+  {
+    id: 3,
+    nombre: "Smod",
+    url: "https://bicicletas.smod.io/",
+    img: "smod.png",
+  },
+  {
+    id: 4,
+    nombre: "Marcopolo",
+    url: "https://www.marcopolo.com.br/es",
+    img: "marcopolo-logo.png",
+  },
+  {
+    id: 5,
+    nombre: "Red Bus",
+    url: "/",
+    img: "LogoRedBus.png",
+  },
+  {
+    id: 6,
+    nombre: "Buswagen",
+    url: "https://buswagen.com.ar/",
+    img: "buswagen_logo.png",
+  },
+];
